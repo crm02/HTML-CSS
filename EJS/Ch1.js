fe5649e6e8f5db0c6aa8c829ceb1d9db1fa7c093
Cody <codyrmill@gmail.com>
2025-09-01 17:34:02 -0400

    aded

---
+++ EJS/Ch1.js
@@ -0,0 +1,19 @@
+// Console.log prints to console.
+console.log('Hello');
+//Declare a variable with let.
+
+let string1 ="Hello1";
+console.log(string1);
+
+let theme = 'light';
+console.log(theme)
+theme = "dark";
+console.log(theme);
+
+let night_theme = "Dark", day_theme ="Bright";
+console.log(night_theme + " " +day_theme);
+
+// Constants are written in lowercase
+const pi = 3.14;
+
+// Functions
